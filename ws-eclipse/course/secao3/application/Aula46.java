4a41cb9cb7ca4af30cb8138874ead141610bb61c
Otávio Anselmo <otavio.aanselmo@gmail.com>
2019-06-23 16:17:43 -0300

    Att

---
+++ ws-eclipse/course/secao3/application/Aula46.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Aula46_Triangle;
+
+public class Aula46 {
+
+	// CRIANDO UM MÉTODO PARA OBTERMOS OS BENEFÍCIOS DE REAPROVEITAMENTO E DELEGAÇÃO
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Aula46_Triangle x, y;
+		x = new Aula46_Triangle();
+		y = new Aula46_Triangle();
+		
+		System.out.println("Enter the measures of triangle X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		System.out.println("Enter the measures of triangule Y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		double areaX = x.area();
+		double areaY = y.area();
+		
+		System.out.printf("Triangle X area: %.4f %n", areaX);
+		System.out.printf("Triangle Y area: %.4f %n", areaY);
+		
+		if (areaX > areaY) {
+			System.out.println("Larger area: X");
+		} else {
+			System.out.println("Larger area: Y");
+		}
+		
+		sc.close();
+
+	}
+
+}
